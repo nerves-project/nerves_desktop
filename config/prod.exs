07bd3e425305f34017ce422809309f99592bcc48
Gus Workman <gworkman@users.noreply.github.com>
2026-04-30 17:36:50 +0200

    fix invalid port error

--- config/prod.exs
+++ config/prod.exs
@@ -12,6 +12,7 @@ config :nerves_desktop, NervesDesktopWeb.Endpoint,
 # known as HSTS. If you have a health check endpoint, you may want to exclude it below.
 # Note `:force_ssl` is required to be set at compile-time.
 config :nerves_desktop, NervesDesktopWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 0],
   force_ssl: [
     rewrite_on: [:x_forwarded_proto],
     exclude: [
